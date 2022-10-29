04c0e7249a6c84bd1ad3e238cb51c0bcc5bdd6b1
mme <9083787+mme022@users.noreply.github.com>
2022-10-29 13:20:11 +0100

    added compound request

--- express-api/app.js
+++ express-api/app.js
@@ -1,23 +1,43 @@
-// const express = require('express');
-import { request, gql } from 'graphql-request'
+const express = require("express");
+const app = express();
+import { request, gql } from "graphql-request";
+
+const axios = require("axios");
+const testAddress = "0x21C8dc59f2E9A11c4C1a0C310641968132c6b1Be";
+
+const port = 3000;
+
+function getCompoundData(address) {
+  return axios.get(
+    `https://api.compound.finance/api/v2/account?addresses[]=${address}`
+  );
+}
+
+app.get(`/`, function (req, res) {
+  getCompoundData(testAddress)
+    .then(function (response) {
+      console.log(response.data.request);
+      res.send(response.data.request);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
 const query = gql`
   {
-    user (id: "0xf48ed9a03fc6bb55949f08649cb54d792928cdfe") {
+    user(id: "0xf48ed9a03fc6bb55949f08649cb54d792928cdfe") {
       id
     }
   }
-`
+`;
 
-request('https://api.thegraph.com/subgraphs/name/aave/protocol-v2', query).then((data) => console.log(data));
+request("https://api.thegraph.com/subgraphs/name/aave/protocol-v2", query).then(
+  (data) => console.log(data)
+);
 
-// const app = express();
-// const port = 3000
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+module.exports = app;

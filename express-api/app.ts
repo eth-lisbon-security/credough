120d1a20889863f945e6ae2e177e0cab1d50465a
Matthew <alexandermatthew12@gmail.com>
2022-10-29 15:42:19 +0100

    Introduce credit scoring algorithm

--- express-api/app.ts
+++ express-api/app.ts
@@ -1,7 +1,7 @@
 import { request, gql } from "graphql-request";
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { getCompoundData } from "./services/compound";
+import { getCompoundData, getScoreOnCompound } from "./services/compound";
 
 const app: Express = express();
 const port = process.env.PORT ?? 3000;
@@ -9,8 +9,14 @@ const port = process.env.PORT ?? 3000;
 const testAddress = "0x21C8dc59f2E9A11c4C1a0C310641968132c6b1Be";
 
 app.get("/", async (req: Request, res: Response) => {
-	const response = await getCompoundData(testAddress);
-	return res.send(response.data.requests);
+	const userAddress = req.query.address;
+
+	if (!userAddress) {
+		res.status(400).send("No address provided");
+		return;
+	}
+	const score = await getScoreOnCompound(userAddress as string);
+	return res.send({ score });
 });
 
 app.listen(port, () => {
--- express-api/services/compound.ts
+++ express-api/services/compound.ts
@@ -1,9 +1,96 @@
 import axios from "axios";
+import { request, gql } from "graphql-request";
+import BigNumber from "bignumber.js";
 
-function getCompoundData(address) {
-	return axios.get(
-		`https://api.compound.finance/api/v2/account?addresses[]=${address}`
-	);
+// TODO: Use "toFixed" to round off the score to 2 decimal places
+// function toFixed(x: string) {
+// 	if (Math.abs(x) < 1.0) {
+// 		var e = parseInt(x.toString().split("e-")[1]);
+// 		if (e) {
+// 			x *= Math.pow(10, e - 1);
+// 			x = "0." + new Array(e).join("0") + x.toString().substring(2);
+// 		}
+// 	} else {
+// 		var e = parseInt(x.toString().split("+")[1]);
+// 		if (e > 20) {
+// 			e -= 20;
+// 			x /= Math.pow(10, e);
+// 			x += new Array(e + 1).join("0");
+// 		}
+// 	}
+// 	return x;
+// }
+
+const BASE_GRAPH_URI =
+	"https://api.thegraph.com/subgraphs/name/graphprotocol/compound-v2";
+
+async function getCompoundData(address: string) {
+	return (
+		await axios.get(
+			`https://api.compound.finance/api/v2/account?addresses[]=${address}`
+		)
+	).data;
 }
 
-export { getCompoundData };
+async function getScoreOnCompound(address: string) {
+	const query = gql`
+  {
+    account(id: "${address}") {
+      id,
+      countLiquidated,
+      countLiquidator,
+      hasBorrowed,
+      health,
+      totalBorrowValueInEth,
+      totalCollateralValueInEth
+    }
+  }
+  `;
+
+	const compoundDetails = await request(BASE_GRAPH_URI, query);
+	console.log("compoundDetails", compoundDetails);
+
+	const {
+		id,
+		countLiquidated,
+		countLiquidator,
+		hasBorrowed,
+		health,
+		totalBorrowValueInEth,
+		totalCollateralValueInEth,
+	} = compoundDetails.account;
+	console.log("hasBorrowed", hasBorrowed);
+
+	// Undefined score
+	if (!hasBorrowed) {
+		return null;
+	}
+
+	const maxRange = 550;
+	const growthRate = 2;
+	const minValue = 300;
+
+	let score =
+		totalCollateralValueInEth /
+		(countLiquidated == 0 ? 1 : countLiquidated * totalBorrowValueInEth);
+	console.log(score);
+
+	const adjustedScore =
+		BigNumber(maxRange)
+			.div(1 + Math.exp(-growthRate * score))
+
+			.toNumber() + minValue;
+
+	/**
+	 * 1. totalCollateralValue -> positive correlation
+	 * 2. totalBorrowValue -> negative correlation
+	 * 3. health -> positive correlation
+	 * 4. countLiquidated -> (dependant)
+	 * 5.
+	 *
+	 */
+
+	return adjustedScore;
+}
+
+export { getCompoundData, getScoreOnCompound };

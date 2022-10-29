019e6bfc3ca8064154df9a511208d95cfc4ebfa8
mme <9083787+mme022@users.noreply.github.com>
2022-10-29 15:28:20 +0100

    added walletconnect

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,28 +1,63 @@
 import type { NextPage } from "next";
-import { Flex, Heading, Input, VStack, Button } from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  Input,
+  VStack,
+  Button,
+  Box,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { useCallback } from "react";
 import axios from "axios";
 
-const Home: NextPage = () => {
-	const handleOnClick = useCallback(() => {
-		// Fetch Credit Score details
-	}, []);
+import {
+  useAccount,
+  ConnectButton,
+  useConnectModal,
+  Web3Button,
+} from "@web3modal/react";
 
-	return (
-		<Flex
-			flexDir="column"
-			alignItems="center"
-			justifyContent="center"
-			height="100vh"
-			backgroundColor="black"
-		>
-			<VStack flexDir="column" spacing="4" mb="4">
-				<Heading>Enter your address here</Heading>
-				<Input placeholder="Enter wallet address" size="md" />
-			</VStack>
-			<Button onClick={handleOnClick}>Submit details</Button>
-		</Flex>
-	);
+const Home: NextPage = () => {
+  let navbarFilter = "none";
+  let navbarBackdrop = "blur(20px)";
+  let navbarShadow = "none";
+  let navbarBg = useColorModeValue(
+    "rgba(244, 247, 254, 0.2)",
+    "rgba(11,20,55,0.5)"
+  );
+  let navbarBorder = "transparent";
+
+  const { isOpen, open, close } = useConnectModal();
+  const { account } = useAccount();
+
+  const handleOnClick = useCallback(() => {
+    // Fetch Credit Score details
+  }, []);
+
+  return (
+    <Flex
+      flexDir="column"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+      backgroundColor="black"
+    >
+      {!account.isConnected ? (
+        <ConnectButton />
+      ) : (
+        <Web3Button />
+        //<button onClick={open}>{account.address}</button>
+      )}
+      {/* or */}
+
+      <VStack flexDir="column" spacing="4" mb="4">
+        <Heading>Enter your address here</Heading>
+        <Input placeholder="Enter wallet address" size="md" />
+      </VStack>
+      <Button onClick={handleOnClick}>Submit details</Button>
+    </Flex>
+  );
 };
 
 export default Home;

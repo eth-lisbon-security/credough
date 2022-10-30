83cdabd8382d50dd445d18b6a4fc1b29d478ff1a
Matthew <alexandermatthew12@gmail.com>
2022-10-30 03:51:53 +0000

    Introduce bounties

--- next/components/header.tsx
+++ next/components/header.tsx
@@ -1,42 +1,32 @@
 import type { NextPage } from "next";
-import {
-  Flex,
-  Heading,
-  Input,
-  VStack,
-  Button,
-  Box,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
 
 import {
-  useAccount,
-  ConnectButton,
-  useConnectModal,
-  Web3Button,
+	useAccount,
+	ConnectButton,
+	useConnectModal,
+	Web3Button,
 } from "@web3modal/react";
 import Image from "next/image";
 
 const Header: NextPage = () => {
-  const { isOpen, open, close } = useConnectModal();
-  const { account } = useAccount();
-  return (
-    <Flex
-      flexDir="row"
-      justifyContent="flex-start"
-      //height="5vh"
-      width="90%"
-      backgroundColor="#38B5DCA1"
-      borderRadius="15px"
-      p={5}
-    >
-      <Image src={"/images/o_donut.png"} width={40} height={40}></Image>
-      <Heading fontFamily={"Chewy"} color="white">
-        Credough
-      </Heading>
-      {!account.isConnected ? <ConnectButton /> : <Web3Button />}
-    </Flex>
-  );
+	const { isOpen, open, close } = useConnectModal();
+	const { account } = useAccount();
+	return (
+		<Flex
+			flexDir="row"
+			justifyContent="space-between"
+			width="80%"
+			backgroundColor="#38B5DCA1"
+			borderRadius="12px"
+		>
+			<Image src={"/images/o_donut.png"} width={40} height={40} alt="donut" />
+			<Heading fontFamily={"Chewy"} color="white">
+				Credough
+			</Heading>
+			{!account.isConnected ? <ConnectButton /> : <Web3Button />}
+		</Flex>
+	);
 };
 
 export default Header;
--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from "next";
 import {
-  Flex,
-  Heading,
-  Input,
-  VStack,
-  Button,
-  Box,
-  useColorModeValue,
+	Flex,
+	Heading,
+	Input,
+	VStack,
+	Button,
+	Box,
+	useColorModeValue,
 } from "@chakra-ui/react";
 import { useCallback } from "react";
 import axios from "axios";
@@ -14,48 +14,34 @@ import axios from "axios";
 import Header from "../components/header";
 import Journey from "../components/journey";
 
-import {
-  useAccount,
-  ConnectButton,
-  useConnectModal,
-  Web3Button,
-} from "@web3modal/react";
+import { useAccount, useConnectModal, Web3Button } from "@web3modal/react";
 
 const Home: NextPage = () => {
-  let navbarFilter = "none";
-  let navbarBackdrop = "blur(20px)";
-  let navbarShadow = "none";
-  let navbarBg = useColorModeValue(
-    "rgba(244, 247, 254, 0.2)",
-    "rgba(11,20,55,0.5)"
-  );
-  let navbarBorder = "transparent";
+	const { isOpen, open, close } = useConnectModal();
+	const { account } = useAccount();
 
-  const { isOpen, open, close } = useConnectModal();
-  const { account } = useAccount();
+	const handleOnClick = useCallback(() => {
+		// Fetch Credit Score details
+	}, []);
 
-  const handleOnClick = useCallback(() => {
-    // Fetch Credit Score details
-  }, []);
-
-  return (
-    <Flex
-      flexDir="column"
-      alignItems="center"
-      justifyContent="center"
-      height="100vh"
-      width="100%"
-      backgroundColor="white"
-    >
-      <Header />
-      <Journey />
-      <VStack flexDir="column" spacing="4" mb="4">
-        <Heading>Enter your address here</Heading>
-        <Input placeholder="Enter wallet address" size="md" />
-      </VStack>
-      <Button onClick={handleOnClick}>Submit details</Button>
-    </Flex>
-  );
+	return (
+		<Flex
+			flexDir="column"
+			alignItems="center"
+			justifyContent="center"
+			height="100vh"
+			width="100%"
+			backgroundColor="white"
+		>
+			<Header />
+			<Journey />
+			<VStack flexDir="column" spacing="4" mb="4">
+				<Heading>Enter your address here</Heading>
+				<Input placeholder="Enter wallet address" size="md" />
+			</VStack>
+			<Button onClick={handleOnClick}>Submit details</Button>
+		</Flex>
+	);
 };
 
 export default Home;

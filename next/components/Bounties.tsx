4e1d2fb46a48ce6d0df25e367ccba2e33ad48591
Matthew <alexandermatthew12@gmail.com>
2022-10-30 02:03:19 +0000

    Introduce bounties isues

--- next/components/Bounties.tsx
+++ next/components/Bounties.tsx
@@ -1,16 +1,36 @@
-import { Button, Flex, Heading, HStack, Image, Box } from "@chakra-ui/react";
+import { Flex, Heading, Image } from "@chakra-ui/react";
 
 const Bounties = () => {
 	return (
-		<Flex flexDir="row" alignItems="center">
+		<Flex flexDir="column" alignItems="center">
 			<Heading>built with</Heading>
-			<Flex flexDir={["column", null, "row"]}>
+			<Flex flexDir={["column", null, "row"]} alignItems="center" gap="24px">
 				<Image src="/images/sommelier.png" alt="Sommelier" />
-				<Image src="/images/ipfs.png" alt="IPFS" />
-				<Image src="/images/wallet-connect.png" alt="Wallet Connect" />
-				<Image src="/images/cronos.png" alt="Cronos" />
-				<Image src="/images/compound.png" alt="Compound" />
-				<Image src="/images/euler.png" alt="Euler" />
+				<Image src="/images/ipfs.png" alt="IPFS" width="100px" height="40px" />
+				<Image
+					src="/images/wallet-connect.png"
+					alt="Wallet Connect"
+					height="35px"
+					width="200px"
+				/>
+				<Image
+					src="/images/cronos.png"
+					alt="Cronos"
+					height="35px"
+					width="120px"
+				/>
+				<Image
+					src="/images/compound.png"
+					alt="Compound"
+					width="200px"
+					height="54px"
+				/>
+				<Image
+					src="/images/euler.png"
+					alt="Euler"
+					width="200px"
+					height="35px"
+				/>
 			</Flex>
 		</Flex>
 	);

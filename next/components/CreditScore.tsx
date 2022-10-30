8e045bd2e7c48cb2198854043f8e36ca50f4eb65
Matthew <alexandermatthew12@gmail.com>
2022-10-30 08:33:34 +0000

    Introduce credit score dashboard

--- next/components/CreditScore.tsx
+++ next/components/CreditScore.tsx
@@ -1,21 +1,101 @@
-import React from "react";
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import React, { useEffect, useMemo } from "react";
+import { Box, Divider, Flex, Heading, Text, VStack } from "@chakra-ui/react";
 
 type Props = {
 	creditScore: number;
+	title: string;
+	metrics?: string[];
+	description: string;
 };
 
-const CreditScoreCard = ({ creditScore }: Props) => {
+const creditScoreToGrade = (creditScore: number) => {
+	if (creditScore <= 300) {
+		return "F";
+	} else if (creditScore <= 400) {
+		return "E";
+	} else if (creditScore <= 450) {
+		return "D";
+	} else if (creditScore <= 550) {
+		return "C";
+	} else if (creditScore <= 650) {
+		return "B";
+	} else if (creditScore <= 850) {
+		return "A";
+	}
+
+	return "Unknown";
+};
+
+const CreditScoreCard = ({
+	creditScore,
+	title,
+	metrics,
+	description,
+}: Props) => {
+	const barWidth = useMemo(() => {
+		const range = creditScore - 300;
+		const percentage = (range / (850 - 300)) * 100;
+		return percentage;
+	}, [creditScore]);
+
+	const grade = creditScoreToGrade(creditScore);
+
 	return (
-		<Flex>
-			<Text>Credit Score</Text>
-			<Flex>
-				<Box>Rating</Box>
-				<Heading>{creditScore}</Heading>
-				<Text>{creditScore} out of 800</Text>
+		<Box
+			flexDir="column"
+			px="12px"
+			py="12px"
+			borderRadius="16px"
+			backgroundColor="white"
+		>
+			<Box>
+				<Heading fontSize="xl">{title}</Heading>
+				<Text>{description}</Text>
+			</Box>
+			<Divider my="3.5" />
+			<Flex flexDir="row" justifyContent="space-between">
+				<Box>
+					<Heading fontWeight="light">Rating</Heading>
+					<Heading fontSize="5xl">{creditScore}</Heading>
+					<Text fontSize="xl">out of 850</Text>
+				</Box>
+				<Flex justifyContent="center" flexDir={"column"}>
+					<Heading fontWeight="light">Grade</Heading>
+					<Heading fontWeight="medium" textAlign="center" fontSize="5xl">
+						{grade}
+					</Heading>
+				</Flex>
 			</Flex>
-			<Box></Box>
-		</Flex>
+			<Box mt="12px" height="48px">
+				<Flex position="relative">
+					<Box
+						position="absolute"
+						height="36px"
+						width="100%"
+						borderRadius="8px"
+						backgroundColor="gray"
+					/>
+					<Box
+						position="absolute"
+						height="36px"
+						width={barWidth}
+						borderRadius="8px"
+						backgroundColor="red"
+					/>
+				</Flex>
+			</Box>
+
+			{(metrics?.length ?? 0) > 0 && (
+				<VStack>
+					<Text fontWeight="bold">Used metrics</Text>
+					{(metrics ?? [])
+						.filter((text) => !!text)
+						.map((data, id) => {
+							return <Text key={data + id}>✔ {data}</Text>;
+						})}
+				</VStack>
+			)}
+		</Box>
 	);
 };
 
--- next/components/Dashboard.tsx
+++ next/components/Dashboard.tsx
@@ -1,26 +1,73 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
 import { Flex, Text } from "@chakra-ui/react";
 import CreditScoreCard from "./CreditScore";
-import { useRouter } from "next/router";
 import { useAccount } from "@web3modal/react";
+import HeaderBase from "./HeaderBase";
+import FooterBase from "./FooterBase";
+import axios from "axios";
 
 const Dashboard = () => {
-	const {
-		account: { isConnected },
-	} = useAccount();
+	const { account } = useAccount();
+	const [onChainScore, setOnChainScore] = React.useState<number | null>(null);
 
-	if (!isConnected) {
-		return (
-			<Flex width="100%" h="100vh" justifyContent="center" alignItems="center">
-				<Text>
-					You are not connected to a wallet. Please connect to a wallet to view
-					your dashboard.
-				</Text>
+	const backendApiUrl = useEffect(() => {
+		(async () => {
+			if (!account.address) return;
+			const response = await axios.get(
+				`http://localhost:3000/?address=${account.address}`
+			);
+			const data = response.data;
+
+			const score = data.score ?? 450;
+			if (!score) return;
+			setOnChainScore(score);
+		})();
+	}, [account.address]);
+
+	const offChainScore = 500;
+	const combinedScore = Math.round(((onChainScore ?? 450) + offChainScore) / 2);
+
+	return (
+		<Flex
+			flexDir="column"
+			alignItems="center"
+			height="100vh"
+			width="100%"
+			className="bg-gradient-to-r from-[#eaf2ff] to-[#d4e4ff]"
+		>
+			<HeaderBase />
+			<Flex width="100%">
+				<Flex flex={1}>
+					<CreditScoreCard
+						creditScore={350}
+						title="Your Overall Credit Score"
+						description="A weighted Average of your On- & Off-Chain Credit Score"
+					/>
+				</Flex>
+				<Flex flex={1}>
+					<CreditScoreCard
+						creditScore={onChainScore!}
+						title="On-Chain Credit Score"
+						description="calculated based on your On-Chain Behaviour & Activity"
+						metrics={[
+							"Total collateral value in ETH",
+							"Total debt in ETH",
+							"Total liquidity for wallet (ETH & USDC)",
+							"Number of times liquidated",
+						]}
+					/>
+				</Flex>
+				<Flex flex={1}>
+					<CreditScoreCard
+						creditScore={350}
+						title="Off-Chain Credit Score"
+						description="calculated based on your Off-Chain Behaviour & Activity"
+					/>
+				</Flex>
 			</Flex>
-		);
-	}
-
-	return <Flex width="100%" h="100vh"></Flex>;
+			<FooterBase />
+		</Flex>
+	);
 };
 
 export default Dashboard;
